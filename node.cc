1f457b7f8868fced9d36b17e064f350b081a1523
kyungim2018 <42262959+kyungim2018@users.noreply.github.com>
2018-08-10 08:23:50 +0000

    Update node.cc
    Node and getdata are done

--- node.cc
+++ node.cc
@@ -3,7 +3,48 @@
 #include "node.h"
 
 Node::Node(char data) {
+  this->val = data; 
+ this->pPriv = nullptr; 
+ this->pNext = nullptr; 
+
   return;
+}
+
+
+
+char Node::GetData() {
+
+  return this->val;
+
+}
+
+
+
+Node* Node::GetPreviousNode() {
+
+  //if there is no previous Node
+
+ if (this->pPriv == nullptr)
+
+  return nullptr;
+
+ else
+
+  return this->pPriv; 
+
+
+
+ // return nullptr;
+
+}
+
+
+
+Node* Node::GetNextNode() {
+
+  //if there is no next Node
+
+ if (this->pNe
 }
 
 char Node::GetData() {
